92513aaf29c8355a419f7cc01d192087f8a4dde5
Ramon <borgesramon1@gmail.com>
2020-11-28 04:24:03 -0300

    projeto-exercito

---
+++ src/css/scripts/main.js
@@ -0,0 +1,10 @@
+git clone //github.com/dsramon1/dsramon1.github.io
+cd dsramon1.github.io
+
+ echo "Hello World"> index.html
+
+ git add --all
+
+ git commit -m "Confirmação inicial"
+
+ git push -u origin master
